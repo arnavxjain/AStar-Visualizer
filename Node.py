b5974e3fd970c9ca1f04f7bdd40199905d928606
arnavjainn06 <jainarnav@outlook.com>
2021-07-14 11:31:42 +0530

    full functionality enabled

--- Node.py
+++ Node.py
@@ -1,19 +1,15 @@
 import pygame
 
-colors = {
-    "red": (255, 0, 0),
-    "blue": (0, 0, 255),
-    "green": (73, 236, 156),
-    "yellow": (255, 255, 0),
-    "white": (255, 255, 255),
-    "black": (85, 85, 85),
-    "purple": (128, 0, 128),
-    "orange": (255, 165, 0),
-    "grey": (128, 128, 128),
-    "turquoise": (73, 183, 236), 
-    "hyper": (34, 34, 34)
-}
-
+RED = (236, 73, 73)
+GREEN = (73, 236, 121)
+BLUE = (240, 255, 0)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (73, 97, 236)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
 
 class Node:
 	def __init__(self, row, col, width, totalRows):
@@ -21,7 +17,7 @@ class Node:
 		self.col = col
 		self.x = row * width
 		self.y = col * width
-		self.color = colors["white"]
+		self.color = WHITE
 		self.neighbors = []
 		self.width = width
 		self.totalRows = totalRows
@@ -30,46 +26,58 @@ class Node:
 		return self.row, self.col
 
 	def isClosed(self):
-		return self.color == colors["red"]
+		return self.color == RED
 
 	def isOpen(self):
-		return self.color == colors["green"]
+		return self.color == GREEN
 
 	def isBarrier(self):
-		return self.color == colors["black"]
+		return self.color == BLACK
 
 	def isStart(self):
-		return self.color == colors["orange"]
+		return self.color == BLUE
 
 	def isEnd(self):
-		return self.color == colors["turquoise"]
+		return self.color == TURQUOISE
 
 	def reset(self):
-		self.color = colors["white"]
+		self.color = WHITE
 
 	def close(self):
-		self.color = colors["red"]
+		self.color = RED
 
-	def open(self):
-		self.color = colors["green"]
+	def setOpen(self):
+		self.color = GREEN
 
 	def setToBarrier(self):
-		self.color = colors["green"]
+		self.color = BLACK
 
 	def setToStart(self):
-		self.color = colors["orange"]
+		self.color = BLUE
 
 	def setToEnd(self):
-		self.color = colors["turquoise"]
+		self.color = TURQUOISE
 
 	def setToPath(self):
-		self.color = colors["purple"]
+		self.color = PURPLE
 
 	def draw(self, win):
 		pygame.draw.rect(win, self.color, (self.x, self.y, self.width, self.width))
 
 	def updateNeighbors(self, grid):
-		pass
+		self.neighbors = []
+
+		if self.row < self.totalRows - 1 and not grid[self.row + 1][self.col].isBarrier(): # DOWN
+			self.neighbors.append(grid[self.row + 1][self.col])
+
+		if self.row > 0 and not grid[self.row - 1][self.col].isBarrier(): # UP
+			self.neighbors.append(grid[self.row - 1][self.col])
+
+		if self.col < self.totalRows - 1 and not grid[self.row][self.col + 1].isBarrier(): # RIGHT
+			self.neighbors.append(grid[self.row][self.col + 1])
+
+		if self.row > 0 and not grid[self.row][self.col - 1].isBarrier(): # LEFT
+			self.neighbors.append(grid[self.row][self.col - 1])
 
 	def __lt__(self, other):
 		return False
--- main.py
+++ main.py
@@ -19,24 +19,78 @@ WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("A* Pathfinding Visualizer")
 
-colors = {
-    "red": (205, 0, 0),
-    "blue": (0, 0, 255),
-    "green": (0, 255, 0),
-    "yellow": (255, 255, 0),
-    "white": (255, 255, 255),
-    "black": (85, 85, 85),
-    "purple": (128, 0, 128),
-    "orange": (255, 165, 0),
-    "grey": (128, 128, 128),
-    "turquoise": (64, 224, 208),
-    "hyper": (34, 34, 34)
-}
+RED = (236, 73, 73)
+GREEN = (73, 236, 121)
+BLUE = (73, 151, 236)
+YELLOW = (255, 255, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+PURPLE = (128, 0, 128)
+ORANGE = (255, 165 ,0)
+GREY = (128, 128, 128)
+TURQUOISE = (64, 224, 208)
 
 def h(p1, p2):
     x1, y1 = p1
     x2, y2 = p2
     return abs(x1 - x2) + abs(y1 - y2)
+
+
+def constructPath(cameFrom, current, draw, start):
+    while current in cameFrom and current != start:
+        current = cameFrom[current]
+        current.setToPath()
+        draw()
+
+
+def algorithm(draw, grid, start, end):
+    count = 0
+    openSet = PriorityQueue()
+    openSet.put((0, count, start))
+
+    cameFrom = {}
+
+    gScore = { node: float("inf") for row in grid for node in row }
+    gScore[start] = 0
+
+    fScore = { node: float("inf") for row in grid for node in row }
+    fScore[start] = h(start.getPos(), end.getPos())
+
+    openSetHash = {start}
+
+    while not openSet.empty():
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+
+        current = openSet.get()[2]
+        openSetHash.remove(current)
+
+        if current == end:
+            constructPath(cameFrom, end, draw, start)
+            end.setToEnd()
+            return True
+
+        for neighbor in current.neighbors:
+            temporaryGScore = gScore[current] + 1
+
+            if temporaryGScore < gScore[neighbor]:
+                cameFrom[neighbor] = current
+                gScore[neighbor] = temporaryGScore
+                fScore[neighbor] = temporaryGScore + h(neighbor.getPos(), end.getPos())
+
+                if neighbor not in openSetHash:
+                    count += 1
+                    openSet.put((fScore[neighbor], count, neighbor))
+                    openSetHash.add(neighbor)
+                    neighbor.setOpen()
+
+        draw()
+
+        if current != start:
+            current.close()
+
+    return False
 
 
 def makeGrid(rows, width):
@@ -56,14 +110,14 @@ def drawGrid(win, rows, width):
     gap = width // rows
 
     for x in range(rows):
-        pygame.draw.line(win, colors["grey"], (0, x * gap), (width, x * gap))
+        pygame.draw.line(win, GREY, (0, x * gap), (width, x * gap))
         
         for x in range(rows):
-            pygame.draw.line(win, colors["grey"], (x * gap, 0), (x * gap, width))
+            pygame.draw.line(win, GREY, (x * gap, 0), (x * gap, width))
 
 
 def draw(win, grid, rows, width):
-    win.fill(colors["hyper"])
+    win.fill(WHITE)
 
     for row in grid:
         for node in row:
@@ -108,19 +162,40 @@ def main(win, width):
 
                 node = grid[row][col]
 
-                if not start:
+                if not start and node != end:
                     start = node
                     start.setToStart()
 
-                elif not end:
+                elif not end and node != start:
                     end = node
                     end.setToEnd()
 
                 elif node != start and node != end:
                     node.setToBarrier()
 
-            if pygame.mouse.get_pressed()[2]:
-                pass
+            elif pygame.mouse.get_pressed()[2]:
+                pos = pygame.mouse.get_pos()
+                row, col = getClickedPos(pos, ROWS, width)
+
+                node = grid[row][col]
+                node.reset()
+                if node == start:
+                    start = None
+                elif node == end:
+                    end = None
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE and start and end:
+                    for row in grid:
+                        for node in row:
+                            node.updateNeighbors(grid)
+
+                    algorithm(lambda: draw(win, grid, ROWS, width), grid, start, end)
+
+                if event.key == pygame.K_c:
+                    start = None
+                    end = None
+                    grid = makeGrid(ROWS, width)
 
     pygame.quit()
 
